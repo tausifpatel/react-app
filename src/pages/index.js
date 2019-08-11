bca19faa0ea2c9242f8dd3f1734e98b54b79bb3a
Raja <raja@rajas-mbp.home>
2019-08-11 18:04:22 +0100

    Changed heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <h1>Learn to design and code react apps</h1>
+      <p>Complete course about best tools and design</p>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
